5a34ce56b6e3dc5f20add95b1171e7d7d51ecde7
haz10em <haz10em@gmail.com>
2024-05-06 06:16:35 +0300

    write the register student's design page now i am so tyred I work from 2 aftarnoon antil now

--- src/app/components/students/new-students/new-students.component.ts
+++ src/app/components/students/new-students/new-students.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
-
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-new-students',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatIconModule],
   templateUrl: './new-students.component.html',
   styleUrl: './new-students.component.scss'
 })
 export class NewStudentsComponent {
 
+  name="info";
 }
